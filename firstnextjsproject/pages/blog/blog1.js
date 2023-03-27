cdcffcaa4c6e534e78caecfe0dfbb95a5848682b
D B Gautam <gautamdb28@gmail.com>
2023-03-27 21:58:14 +0545

    Next.js nested file based routing

---
+++ firstnextjsproject/pages/blog/blog1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function blog1() {
+  return (
+    <div>
+        <h1>My Blog 1 Content</h1>
+    </div>
+  )
+}
---
+++ firstnextjsproject/pages/blog/blog2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function blog2() {
+  return (
+    <div>
+        <h1>My Blog 2 Content</h1>
+    </div>
+  )
+}
