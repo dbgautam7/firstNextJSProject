6d99a23dcea81120e1ce5be248480ca71dc37a9b
D B Gautam <gautamdb28@gmail.com>
2023-03-28 15:37:27 +0545

    data fetch from dummy.json api using getStaticProps method

--- firstnextjsproject/pages/blog.js
+++ firstnextjsproject/pages/blog.js
@@ -1,11 +1,40 @@
 import Navbar from '@/components/navbar'
 import styles from '@/styles/home.module.css'
 
-const Blog = () => {
+export const getStaticProps=async()=> {
+  const res = await fetch('https://dummyjson.com/products')
+  const data = await res.json()
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
+
+const Blog = ({data}) => {
+  console.log(data,"data")
   return (
     <div className={styles.blog}>
       <Navbar />
         <p>This is my Blog Page</p>
+        <div>
+        {data.products.slice(0,5).map((item,id)=>{
+          return (
+            <div key={id}>
+              <ul>
+                <li>
+               <h3>{item.id}</h3>
+            <h3>{item.title}</h3>
+            </li>
+            </ul>
+            </div>
+          )
+        })}
+        </div>
     </div>
   )
 }
